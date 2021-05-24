13ca3970d4e6259f2905c2bfd29480e9bfe28e66
DimaChekashov <foxdima99@gmail.com>
2021-05-24 13:23:49 +0300

    add google map

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
 import { getCity } from "../api/openApi.js";
 
-function App() {
+function App(props) {
     const cityId = "465543";
     const [cityWeather, setCityWeather] = useState({});
 
@@ -11,7 +12,21 @@ function App() {
         getCity(cityId).then(setCityWeather).catch(console.error);
     }, []);
 
-    return <div className="App">Hello {cityWeather.name}!</div>;
+    return (
+        <div className="App">
+            <h1>Hello {cityWeather.name}!</h1>
+            <Map
+                google={props.google}
+                zoom={8}
+                initialCenter={{
+                    lat: 40.854885,
+                    lng: -88.081807,
+                }}
+            ></Map>
+        </div>
+    );
 }
 
-export default App;
+export default GoogleApiWrapper({
+    apiKey: "AIzaSyCwm3DSlBLwoMrFgPniKHlxYXna3poxV6Y",
+})(App);
