53d588d9ee68ffe02d9df143bf64afcb7498d8ba
DimaChekashov <foxdima99@gmail.com>
2022-04-25 17:43:26 +0300

    update dashboard

--- foxsay-weather_v2/src/pages/Dashboard/Dashboard.tsx
+++ foxsay-weather_v2/src/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { observer } from "mobx-react";
 import { Link } from "react-router-dom";
-import getWeather from "../../api/openApi";
 import addIcon from "../../assets/add-icon.png";
 import CityCart from "../../components/CityCart/CityCart";
 import Weather from "../../store/Weather";
-import Loader from "../../components/Loader/Loader";
 import "./Dashboard.sass";
 
 interface Props{
@@ -13,43 +10,22 @@ interface Props{
 }
 
 interface State {
-    isLoaded: boolean,
-    city: any
 }
 
-@observer export default class Dashboard extends React.Component<Props, State> {
+export default class Dashboard extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
-        this.state = {
-            isLoaded: false,
-            city: {}
-        }
+        this.state = { }
     }
 
     componentDidMount() {
-        getWeather
-            .getCity(465543)
-            .then((city) => {
-                this.setState({
-                    city,
-                    isLoaded: true
-                })
-            })
-            .catch(console.error);
-            
-        console.log(Weather.cities[0].name);
+
     }
 
     render() {
-        const { city, isLoaded } = this.state;
-
-        if (!isLoaded) {
-            return <Loader />;
-        }
-
         return (
             <div className="cart-grid">
-                {Weather.cities.map((cityItem) => {
+                {Weather.cities ? Weather.cities.map((cityItem) => {
                     return (
                         <CityCart
                             key={cityItem.id}
@@ -62,7 +38,8 @@ interface State {
                             link={`/city/${cityItem.id}`}
                         />
                     );
-                })}
+                }) : ""}
+
                 <Link to="/add-city" className="add-cart fadeInBottom">
                     <img src={addIcon} alt="add icon" />
                 </Link>
